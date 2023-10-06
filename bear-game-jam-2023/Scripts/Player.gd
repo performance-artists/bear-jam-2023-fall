dd3d1e39ab82fa5ccf5fa661dfc212bc107e22c2
ItzCog <70334983+ItzCog@users.noreply.github.com>
2023-10-06 18:15:14 +0800

    fixed goofy rotation

--- bear-game-jam-2023/Scripts/Player.gd
+++ bear-game-jam-2023/Scripts/Player.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 
 var SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const ROTATE_SPEED = 10
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -11,6 +12,7 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var visual = $Visual
 
 var walking = false
+var look_direction = Vector3.ZERO
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -26,10 +28,12 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		look_direction = look_direction.lerp(direction, delta * ROTATE_SPEED).normalized()
+		
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 		
-		visual.look_at(direction + position)
+		visual.look_at(look_direction + position)
 		
 		walking = true
 		
