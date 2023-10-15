a848465c2fae584e28fb87427197e2343bbe439f
Nick MC <n1ch0l45wk@gmail.com>
2023-10-15 20:31:13 +0800

    Scene Transition Anim

--- bear-game-jam-2023/Scripts/PlayButton.gd
+++ bear-game-jam-2023/Scripts/PlayButton.gd
@@ -1,4 +1,11 @@
 extends Button
 
+@onready var animation_player = $"../AnimationPlayer"
+
+func _ready():
+	animation_player.play_backwards("fade")
+
 func _on_pressed():
+	animation_player.play("fade")
+	await animation_player.animation_finished
 	get_tree().change_scene_to_file("res://Scenes/Nick.tscn")
--- bear-game-jam-2023/ToMenu.gd
+++ bear-game-jam-2023/ToMenu.gd
@@ -1,5 +1,11 @@
 extends Button
 
-func _on_pressed():
+@onready var animation_player = $"../../AnimationPlayer"
 
+func _ready():
+	animation_player.play_backwards("fade")
+
+func _on_pressed():
+	animation_player.play("fade")
+	await animation_player.animation_finished
 	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
