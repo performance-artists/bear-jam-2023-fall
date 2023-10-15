0f0d301be364950bbc0a251d8fe8652b36f86bff
Yuanche Ma <70334983+ItzCog@users.noreply.github.com>
2023-10-15 13:11:47 +0800

    added damaged smoke

--- bear-game-jam-2023/Scripts/Player.gd
+++ bear-game-jam-2023/Scripts/Player.gd
@@ -11,6 +11,7 @@ extends RigidBody3D
 @onready var car_taxi = $CarMesh/RootNode/car_taxi
 @onready var car_taxi_destroyed = $CarMesh/RootNode/car_taxi_destroyed
 @onready var explosion = $Explosion
+@onready var smoke = $Smoke
 
 # Where to place the car mesh relative to the sphere
 var sphere_offset = Vector3.DOWN
@@ -40,6 +41,7 @@ var destroyed = false
 func _ready():
 	starting_running_sfx_volume = running_sfx_player.volume_db
 	randomize()
+	smoke.emitting = false
 
 func _physics_process(delta):
 	car_mesh.position = sphere_offset
@@ -66,6 +68,9 @@ func _physics_process(delta):
 		running_sfx_player.volume_db = starting_running_sfx_volume - 80
 
 func _process(delta):
+	if GlobalHealth.value <= 40:
+		smoke.emitting = true
+	
 	brake_sfx_timer -= delta
 	acceleration -= 1
 	if acceleration < 15:
