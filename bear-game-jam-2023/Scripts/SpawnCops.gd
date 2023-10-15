60f8203a70e8e9569bbe8e506d37513cb8e7a6fd
MCSC2021 <80908717+MCSC2021@users.noreply.github.com>
2023-10-16 00:13:04 +0800

    Random COP spawn location

--- bear-game-jam-2023/Scripts/SpawnCops.gd
+++ bear-game-jam-2023/Scripts/SpawnCops.gd
@@ -4,11 +4,13 @@ var spawn_cooldown = 2.5
 var cooldown = 5
 
 var cop_model = load("res://Player/Cop.tscn")
+@onready var Target = $"../Car"
 
 func _process(delta):
 	cooldown += delta
 	if cooldown > spawn_cooldown and PoliceManager.current_police_total < PoliceManager.police_limit:
 		PoliceManager.current_police_total += 1
 		cooldown = 0
+		global_position = Target.global_position + Vector3(randi() % 30, 0.186, randi() % 30)
 		var cop = cop_model.instantiate()
 		add_child(cop)
