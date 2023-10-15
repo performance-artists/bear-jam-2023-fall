379dce2b7cf7f0b37de4d3c123f01a36a3f8cbfc
Dorjderem2002 <dorjderemdorj202@gmail.com>
2023-10-16 00:01:08 +0800

    road

--- bear-game-jam-2023/Scripts/npc.gd
+++ bear-game-jam-2023/Scripts/npc.gd
@@ -1,13 +1,18 @@
 extends Area3D
 
-const move_speed = 5
+const move_speed = 1
+const del_time = 10
+var temp_time = 0
 
 func _ready():
-	pass
+	temp_time = del_time
 
 func _process(delta):
-	position = position + Vector3(0,0,move_speed)
-
+	position = position + Vector3(0,0,move_speed * delta)
+	if temp_time <= 0:
+		queue_free()
+		temp_time = del_time
+	temp_time -= delta
 
 func _on_body_entered(body):
 	if body.name == "Car":
