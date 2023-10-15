cca5dd9cf303bccb1e1dfd7ddaad1fbda48a039b
Nick MC <n1ch0l45wk@gmail.com>
2023-10-16 03:34:58 +0800

    final?

--- bear-game-jam-2023/Scripts/health_box.gd
+++ bear-game-jam-2023/Scripts/health_box.gd
@@ -2,13 +2,18 @@ extends Area3D
 
 @onready var audio_stream_player = $AudioStreamPlayer
 
+var check = true
+
 func _on_body_entered(body):
-	print(body)
-	if GlobalHealth.value <= 80:
-		GlobalHealth.value += 20
-	else:
-		GlobalHealth.value = 100
-	audio_stream_player.play()
-	hide()
-	await get_tree().create_timer(30.0).timeout
-	show()
+	if check:
+		check = false
+		print(body)
+		if GlobalHealth.value <= 80:
+			GlobalHealth.value += 20
+		else:
+			GlobalHealth.value = 100
+		audio_stream_player.play()
+		hide()
+		await get_tree().create_timer(30.0).timeout
+		show()
+		check = true
